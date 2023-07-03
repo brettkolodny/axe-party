d5e04c66779bbe26d13597b1d1008dbadc9384ef
brettkolodny <brettkolodny@gmail.com>
2023-07-03 12:25:08 -0400

    feat: add pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import elmPlugin from "vite-plugin-elm";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [elmPlugin()],
+  plugins: [elmPlugin(), VitePWA()],
 });
